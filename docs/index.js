860e65496045ef80f149a6e46d78c34a597aa0ec
Michel Machado <michelhmachado@gmail.com>
2024-05-14 20:29:24 -0300

    feat: updated index.js to work out of extension context

--- docs/index.js
+++ docs/index.js
@@ -1,12 +1,21 @@
-chrome.runtime.sendMessage({
-  type: 'oauth',
-  url: window.location.href,
-});
+// Function to get the value of a URL parameter
+function getParameterByName(name, url = window.location.href) {
+  name = name.replace(/[[\]]/g, '\\$&');
+  var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+    results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
 
-chrome.runtime.onMessage.addListener(message => {
-  console.log('message from ouath.js: ', message);
-});
+const code = getParameterByName('code');
+if (code) {
+  // Construct the URL to open your extension's popup
+  const extensionURL = `chrome-extension://${chrome.runtime.id}/src/pages/popup/Popup.tsx?code=${code}`;
 
-setTimeout(() => {
-  window.close();
-}, 5000);
+  // Redirect to the extension popup
+  window.location.href = extensionURL;
+} else {
+  // Handle case where 'code' is not present
+  document.body.innerHTML = '<h1>Error: OAuth code not found.</h1>';
+}
